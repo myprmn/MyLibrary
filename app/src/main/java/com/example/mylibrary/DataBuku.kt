1ca114753d7087491d1f2922023f1bd80c244478
PC-05 <yusuppermana19@gmail.com>
2022-06-03 17:05:10 +0700

    MyLibrary

---
+++ app/src/main/java/com/example/mylibrary/DataBuku.kt
@@ -0,0 +1,11 @@
+package com.example.mylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataBuku : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_buku)
+    }
+}
